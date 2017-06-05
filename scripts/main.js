b4b0cd64585874e63773863c6daaf86aeb41a499
Mathilde Fournier <thilde.mf@gmail.com>
2017-06-05 16:59:20 +0200

    Prions

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,7 @@ var bloc1 = document.querySelector(".menu1");
 var bloc2 = document.querySelector(".menu2");
 var bloc3 = document.querySelector(".menu3");
 var bloc4 = document.querySelector(".menu4");
+var text = document.querySelector(".openMenu");
 
 img1.addEventListener(
   'click',
@@ -18,18 +19,19 @@ img1.addEventListener(
     if(bloc1.classList.contains("is-active"))
       {
         img2.setAttribute("src", "../../images/menu2.jpg");
+        menu2.classList.add(".textMenu2");
         bloc2.style.width=("25%");
         bloc2.style.float=("left");
         
-        img3.setAttribute("src", "../../images/menu3mini.jpg");
-        menu3.innerHTML=("");
-        bloc3.style.width=("100px");
-        bloc3.style.float=("right");
-
-        img4.setAttribute("src", "../../images/vinemini.jpg");
-        menu4.innerHTML=("");
-        bloc4.style.width=("100px");
-        bloc4.style.float=("right");
+        img3.setAttribute("src", "../../images/menu3.jpg");
+        menu3.classList.add(".textMenu3");
+        bloc3.classList.remove(".onclick");
+        
+        img4.setAttribute("src", "../../images/vine.jpg");
+        menu4.classList.add(".textMenu2");
+        bloc4.classList.remove(".onclick");
+        
+        text.style.display=("none");
       }
     else
       {
@@ -47,6 +49,8 @@ img1.addEventListener(
         menu4.innerHTML=("");
         bloc4.style.width=("100px");
         bloc4.style.float=("right");
+        
+        text.style.display=("block");
       }
     bloc1.classList.toggle("is-active");
   },
